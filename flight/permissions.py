f5cf2dd679fe7204ff7c67c0c2c57115c87542ff
esadakman <esadd26@gmail.com>
2022-09-30 14:15:03 +0300

    permissions.py created

---
+++ flight/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework import permissions
+
+# ! flightların sadece yetkili kişiler tarafından oluşturulması ve değiştirilmesi için permission.py dosyasını oluşturduk
+class IsStafforReadOnly(permissions.IsAdminUser):
+    
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return bool(request.user and request.user.is_staff)
+
+# ? oluşturduğumuz permisson'ı views.py'a gidip import ediyoruz
